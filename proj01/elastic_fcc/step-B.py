1f079805dc012d8049a3258f772997fac2483bca
impurity <impurity@postech.ac.kr>
2017-02-20 16:06:01 +0900

    elastic constants

---
+++ proj01/elastic_fcc/step-B.py
@@ -0,0 +1,103 @@
+
+import csv
+import os
+from ase import Atom, Atoms
+from ase.lattice.cubic import *
+from ase.visualize import view
+from numpy import *
+from emto import *
+from ase.utils.eos import EquationOfState
+import matplotlib.pyplot as plt
+from ase.lattice import bulk
+from ase.units import *
+from mpi4py import MPI
+
+comm = MPI.COMM_WORLD
+size = comm.Get_size()
+rank = comm.Get_rank()
+
+print rank, size
+
+name = 'B'
+
+curr_dir = os.getcwd()
+
+temp_dir = curr_dir.replace('work','temp')
+
+os.system('mkdir {0}/graph'.format(temp_dir))
+os.system('mkdir {0}/result'.format(temp_dir))
+
+result = '{0}/result/result-{1}.txt'.format(temp_dir,name)
+os.system('rm {0}'.format(result))
+
+result_sum = '{0}/result/summary-{1}.csv'.format(temp_dir,name)
+os.system('rm {0}'.format(result_sum))
+
+save(result, '{0}'.format(name))
+save(result_sum, '{0}'.format(name))
+
+OPTIONS = np.linspace(0.98, 1.02, 9)
+volumes = []
+energies = []
+
+cr = 0.15
+ni = 0.15
+fe = 1.0-cr-ni
+
+for opt in OPTIONS:
+
+    l = 3.602 * opt
+    atoms = bulk('Fe', 'fcc', a=l)
+
+    atoms.set_tags([1])
+
+    alloys = []
+    alloys.append(Alloy(1, 'Fe', fe / 2, 1.0))
+    alloys.append(Alloy(1, 'Fe', fe / 2, -1.0))
+    alloys.append(Alloy(1, 'Cr', cr / 2, 1.0))
+    alloys.append(Alloy(1, 'Cr', cr / 2, -1.0))
+    alloys.append(Alloy(1, 'Ni', ni / 2, 1.0))
+    alloys.append(Alloy(1, 'Ni', ni / 2, -1.0))
+
+    calc = EMTO()
+    calc.set(dir='{0}/calc/{1}/opt-{2:0.3f}'.format(temp_dir, name, opt),
+             lat=2,
+             ncpa=20,
+             amix=0.05,
+             afm='F',
+             kpts=[13, 13, 13],
+             )
+    calc.set_alloys(alloys)
+
+    atoms.set_calculator(calc)
+    nm_e = atoms.get_potential_energy() / atoms.get_number_of_atoms()
+    nm_v = atoms.get_volume() / atoms.get_number_of_atoms()
+
+    if nm_e < -0.001:
+        volumes.append(nm_v)
+        energies.append(nm_e)
+
+    save(result, '{3} result : {0} {1} {2}'.format(opt, nm_v, nm_e, name))
+
+print volumes, energies
+
+eos = EquationOfState(volumes, energies)
+v0, e0, B = eos.fit()
+eos.plot('graph/{0}.png'.format(name))
+
+save(result, '{0} {1} {2} {3}'.format(v0, e0, B/kJ*1.0e24, (4.0 * v0) ** (1.0 / 3.0)))
+
+save(result, OPTIONS)
+save(result, volumes)
+save(result, energies)
+
+save(result, '------------------------')
+
+save(result_sum, '{0}, {1}, {2}, {3}, {4}, {5}'.format(name, e0, v0, B, volumes, energies))
+
+
+
+
+
+
+
---
+++ proj01/elastic_fcc/step-C44.py
@@ -0,0 +1,130 @@
+
+import csv
+import os
+from ase import Atom, Atoms
+from ase.lattice.cubic import *
+from ase.visualize import view
+from numpy import *
+from emto import *
+from ase.utils.eos import EquationOfState
+import matplotlib.pyplot as plt
+from ase.lattice import bulk
+from mpi4py import MPI
+from ase.units import *
+import numpy.polynomial.polynomial as poly
+
+comm = MPI.COMM_WORLD
+size = comm.Get_size()
+rank = comm.Get_rank()
+
+print rank, size
+
+name = 'C44'
+
+curr_dir = os.getcwd()
+os.system('mkdir eos')
+os.system('mkdir result')
+
+temp_dir = curr_dir.replace('work','temp')
+
+result = '{0}/result/result-{1}.txt'.format(curr_dir,name)
+os.system('rm {0}'.format(result))
+
+result_sum = '{0}/result/summary-{1}.csv'.format(curr_dir,name)
+os.system('rm {0}'.format(result_sum))
+
+save(result, '{0}'.format(name))
+save(result_sum, '{0}'.format(name))
+
+OPTIONS = np.linspace(0, 0.05, 6)
+volumes = []
+energies = []
+
+cr = 0.15
+ni = 0.15
+fe = 1.0-cr-ni
+
+for opt in OPTIONS:
+
+    l = 3.602/np.sqrt(2)
+    atoms = bulk('Fe', 'bcc', a=l)
+
+    scale = [[1,0,0],[0,1,0],[0,0,np.sqrt(2)]]
+    atoms.set_cell(np.dot(scale, atoms.get_cell()), scale_atoms=True)
+
+    print atoms.get_cell()
+
+    atoms.set_tags([1])
+
+    alloys = []
+    alloys.append(Alloy(1, 'Fe', fe / 2, 1.0))
+    alloys.append(Alloy(1, 'Fe', fe / 2, -1.0))
+    alloys.append(Alloy(1, 'Cr', cr / 2, 1.0))
+    alloys.append(Alloy(1, 'Cr', cr / 2, -1.0))
+    alloys.append(Alloy(1, 'Ni', ni / 2, 1.0))
+    alloys.append(Alloy(1, 'Ni', ni / 2, -1.0))
+
+#    dist = [[1+opt, 0, 0], [0, 1+opt, 0], [0, 0, 1/(1+opt)**2]]
+    dist = [[1+opt, 0 , 0], [0, 1-opt, 0], [0, 0, 1 / (1 - opt ** 2)]]
+
+    atoms.set_cell(np.dot(dist, atoms.get_cell()), scale_atoms=True)
+
+    print atoms.get_cell()
+
+    calc = EMTO()
+    calc.set(dir='{0}/{1}/opt-{1:0.3f}'.format(temp_dir, name, opt),
+             lat=10,
+             ncpa=20,
+             amix=0.05,
+             afm='F',
+             kpts=[13, 13, 13],
+             dmax=2.40
+             )
+    calc.set_alloys(alloys)
+
+    atoms.set_calculator(calc)
+
+    nm_e = 0
+    nm_e = atoms.get_potential_energy() / atoms.get_number_of_atoms()
+    nm_v = atoms.get_volume() / atoms.get_number_of_atoms()
+
+    if nm_e < -0.001:
+        volumes.append(nm_v)
+        energies.append(nm_e)
+
+    save(result, '{3} result : {0} {1} {2}'.format(opt, nm_v, nm_e, name))
+
+print volumes, energies
+
+
+OPTIONS = (-1.0*OPTIONS[::-1]).tolist() + OPTIONS.tolist()
+energies = (energies[::-1]) + energies
+
+
+coefs = poly.polyfit(OPTIONS, energies, 3)
+
+C44 = coefs[2]/volumes[0]/kJ*1.0e24
+
+print C44
+
+x_new = np.linspace(OPTIONS[0]-0.01, OPTIONS[-1]+0.01, num=len(OPTIONS)*10)
+
+ffit = poly.polyval(x_new, coefs)
+
+plt.scatter(OPTIONS, energies)
+plt.plot(x_new, ffit)
+plt.savefig('C44.png')
+
+save(result, OPTIONS)
+save(result, volumes)
+save(result, energies)
+
+save(result, '------------------------')
+
+save(result_sum, '{0}, {1}, {2}, {3}'.format(name, C44, volumes, energies))
+
+
+
+
+
+
